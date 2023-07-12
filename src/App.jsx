b35200c0913e85b353e0abe23d1b880e3b4d75b6
react <solangeroman571@gmail.com>
2023-07-12 20:27:56 +0200

    mi primer proyecto con react

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { RecipeTitle } from "./components/RecipeTitle";
+import { RecipeImage } from "./components/RecipeImage";
+import { IngredientList } from "./components/IngredientList";
+import { RecipeSteps } from "./components/RecipeSteps";
+import { initialRecipes } from "./initialRecipes";
+
+export default function App() { 
+ const [recipe, setRecipe] = useState(initialRecipes[0]);
+ const [prepared, setPrepared] = useState(false);
+    //const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        setPrepared(recipe.ingredients.every((i) => i.prepared));
+    }, [recipe]);
+
+    const handleIngredientClick = (index) => {
+        const updateRecipe = { ...recipe}; //crear una copia primero
+        updateRecipe.ingredients[index].prepared = !updateRecipe.ingredients[index].prepared;
+        setRecipe(updatedRecipe);
+    };
+
+    const { ingredients, image, title, feedback, steps } = recipe;
+    
+    return (
+        <article className="container">
+         
+          <RecipeImage image={image} title={title} />
+          <RecipeTitle title={title} feedback={feedback} />
+          <IngredientList
+            ingredients={ingredients}
+            onIngredientClick={handleIngredientClick}
+          />
+          <h2 className="subtitle">
+            {prepared ? "👨‍🍳 Prep work done!" : "🍳 Just Keep chopping"}
+          </h2>
+          <RecipeSteps steps={steps} />
+         
+        </article>
+      );
+    }
+    
+
+        
